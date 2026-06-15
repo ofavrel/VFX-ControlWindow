a8046cd39ad2794ff0ad2e7f2e76311411bca3cd
Orson <orson.favrel@gmail.com>
2026-06-15 21:28:41 +0200

    getting rid of VFXControl folder and all asset. Consuming VFXControl with UPM package

--- Assets/VfxControl/Editor/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-// Exposes the tool's `internal` types (VfxGraphReflection, VfxPropertySheet, VfxControlState, …)
-// to the EditMode test assembly only. No effect on production behavior.
-
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("VfxControl.Editor.Tests")]
